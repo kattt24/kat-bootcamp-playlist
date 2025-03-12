c84da490d982bbd2c7f1f893e8d0f06d7c386b2d
katia <katiab24@berkeley.edu>
2025-03-11 23:43:59 -0700

    switching colors

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-    <header className="bg-yellow-100 text-pink-400 py-4 px-6 flex justify-between items-center w-full fixed top-0 left-0">
+    <header className="bg-pink-400 text-white py-4 px-6 flex justify-between items-center w-full fixed top-0 left-0">
     <h1 className="text-lg font-bold text-pink-600">Playlist Creator</h1>
     <div className="flex space-x-6">
         <p className="hover:text-gray-400 cursor-pointer">Home</p>
