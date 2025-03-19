8b6b90d6ecc905ce4b6705829f40c11cc7f151f8
Katia Bernabe <kat@Katias-MacBook-Air.local>
2025-03-19 15:36:38 -0700

    fixing the colors pt 2

--- src/pages/playlists/index.tsx
+++ src/pages/playlists/index.tsx
@@ -4,11 +4,11 @@ import { playlists } from "@/data/example_data";
 export default function PlaylistsPage() {
     return (
     <div className="min-h-screen bg-pink-100 text-white p-8">
-      <h1 className="text-3xl font-bold mb-6">Playlists</h1>
+      <h1 className="text-3xl text-pink-600 font-bold mb-6">Playlists</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {playlists.map((playlist) => (
           <Link key={playlist.id} href={`/playlists/${playlist.id}`}>
-            <div className="p-6 bg-gray-800 rounded-lg shadow-lg cursor-pointer hover:bg-gray-700 transition">
+            <div className="p-6 bg-pink-300 rounded-lg shadow-lg cursor-pointer hover:bg-gray-700 transition">
               <h2 className="text-xl font-semibold">{playlist.title}</h2>
               <p className="text-gray-400">{playlist.description}</p>
               <p className="text-gray-300 mt-2">{playlist.songs.length} songs</p>
